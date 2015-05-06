0915f221207d2383367c1af8588800772fdd6816
tuttarealstep <fste999@live.it>
2015-05-06 17:26:44 +0200

    Revert "MyCMS 0.0.5.0"
    This reverts commit 537e79c8d953802df6bc0128c0729a260ca6b447.

--- app/content/theme/default/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*                     *\
-|	MYCMS - TProgram    |
-\*                     */
-global $my_users;
-
-if(!isset($_GET['return_url'])){ $return_url = ""; } else { $return_url = my_sql_secure(base64_decode($_GET['return_url'])); $return_url_und = my_sql_secure($_GET['return_url']);}
-$my_users->logout($return_url);
-?>
