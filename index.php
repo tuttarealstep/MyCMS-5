bd7e9f136df854824c72de98d0951ffca0dca101
Stefano <fste999@gmail.com>
2015-05-06 17:53:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 /*                     *\
 |	MYCMS - TProgram    |
 \*                     */
-/**
- *  @package Index
- */
 
 define('FILE', dirname( __FILE__ ));
 define('MY_INSTALL', '/install/');
@@ -13,6 +10,5 @@ if(file_exists(FILE.MY_INSTALL.'index.php')):
     header("location: ../install/index.php");
     exit;
 else:
-    //This file only loads loader.php
     require_once( dirname( __FILE__ ) . '/loader.php' );
 endif;
